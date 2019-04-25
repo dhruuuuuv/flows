0bceaf1b871b9d3cbffb891c9ba6497c09800cc8
Dhruv Chauhan <dhruvchauhan@nicepeopleatwork.com>
2019-04-25 15:05:32 +0200

    ADD: changed main component structure

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -2,9 +2,9 @@ import { store } from './store'
 
 import React from 'react'
 import ReactDom from 'react-dom'
-import { Dashboard } from './components/Dashboard'
+import { Main } from './components/Main'
 
 ReactDom.render(
-    <Dashboard />,
+    <Main />,
     document.getElementById('app')
 )
